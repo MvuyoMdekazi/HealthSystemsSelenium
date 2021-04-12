3ad278f5e74d0881bd48f795da2808b3ecafeb18
Mvuyo <Mvuyo@HSTCPT-Mvuyo.HST.local>
2021-04-12 15:10:20 +0200

    mvuyo first commit

---
+++ MyfirstRepo/src/main/java/com/org/MyfirstRepo/MyfirstRepo/NewClassDemoget.java
@@ -0,0 +1,12 @@
+package com.org.MyfirstRepo.MyfirstRepo;
+
+public class NewClassDemoget {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Mvuyo");
+
+	}
+
+}
